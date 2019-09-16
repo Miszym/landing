4e742e4f5bc8a3fbeb03f01ec27e76f67a625b1b
Michal <michal@nonexistdsfsdf.bbvcx>
2019-09-16 22:33:03 +0200

    Updates

---
+++ precache-manifest.448be997c366ad7087c7aa27b601320e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e01a1237a862e1958a84132f5b51033",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "bd0cce00ca6b74d8bc13",
+    "url": "/landing/static/css/main.433b13b4.chunk.css"
+  },
+  {
+    "revision": "5289b26ebae684ee407e",
+    "url": "/landing/static/js/2.905c666f.chunk.js"
+  },
+  {
+    "revision": "bd0cce00ca6b74d8bc13",
+    "url": "/landing/static/js/main.3bd9bc0b.chunk.js"
+  },
+  {
+    "revision": "a2d6b8eee8f19b9d4c47",
+    "url": "/landing/static/js/runtime~main.ea54c4f4.js"
+  },
+  {
+    "revision": "c1c2747686f3e49b2aa2e4fea4d054b5",
+    "url": "/landing/static/media/discover.c1c27476.svg"
+  },
+  {
+    "revision": "ab17c3cb8697c4aed0b6abd90431796c",
+    "url": "/landing/static/media/facebook.ab17c3cb.svg"
+  },
+  {
+    "revision": "eb3dde60e0921e3458f4bd05b383bfd5",
+    "url": "/landing/static/media/home.eb3dde60.svg"
+  },
+  {
+    "revision": "39a74203356a4291582aa0ac1f593dd1",
+    "url": "/landing/static/media/instagram.39a74203.svg"
+  },
+  {
+    "revision": "c1021f469e050c1cc82d588a9b04c08e",
+    "url": "/landing/static/media/mail.c1021f46.svg"
+  },
+  {
+    "revision": "0a7bec2411f30df81fdd05d34d8af408",
+    "url": "/landing/static/media/menuIcon.0a7bec24.svg"
+  },
+  {
+    "revision": "19067c44af756dfa85bd6f6f7c0c4f0c",
+    "url": "/landing/static/media/newsMain.19067c44.png"
+  },
+  {
+    "revision": "3291b9a373e25483af98d236dd683bec",
+    "url": "/landing/static/media/photos.3291b9a3.svg"
+  },
+  {
+    "revision": "04c948e1b76bd46c93aeb9edf54e762a",
+    "url": "/landing/static/media/send.04c948e1.svg"
+  },
+  {
+    "revision": "396b9d065637f0d661ece78a027b612e",
+    "url": "/landing/static/media/time.396b9d06.svg"
+  },
+  {
+    "revision": "defa8c88bb971ddf10b47a3955ee2bee",
+    "url": "/landing/static/media/trending1.defa8c88.png"
+  },
+  {
+    "revision": "0f72ee4f79c03d77f3655df838ce887f",
+    "url": "/landing/static/media/trending2.0f72ee4f.png"
+  },
+  {
+    "revision": "1f29823423bdda8c8ec6128c539557a6",
+    "url": "/landing/static/media/trending3.1f298234.png"
+  },
+  {
+    "revision": "734129c3a3e9392941ec1ccfd0c1627f",
+    "url": "/landing/static/media/twitter.734129c3.svg"
+  },
+  {
+    "revision": "657e10c280924b9805454a2eaef0eb07",
+    "url": "/landing/static/media/web.657e10c2.svg"
+  }
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import LandingPage from "./landingPage/LandingPage"
-
-ReactDOM.render(<LandingPage></LandingPage>, document.getElementById("root"))
